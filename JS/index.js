709516e31bff7e69da6c02524ae33e29770d5084
Arick Hasan <hasanarick000@gmail.com>
2022-03-02 21:27:48 +0600

    9th

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,15 @@
 const errorMassage = document.getElementById("error");
 errorMassage.style.display = "none";
+const searchResult = document.getElementById("search-result");
+const mobileDetails = document.getElementById("display-mobile-details");
 const searchMobile = () => {
   const mobileSearchField = document.getElementById("mobile-search-field");
   const searchText = mobileSearchField.value;
   mobileSearchField.value = "";
   if (searchText == "") {
     errorMassage.style.display = "block";
+    searchResult.textContent = "";
+    mobileDetails.textContent = "";
   } else {
     errorMassage.style.display = "none";
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
@@ -15,10 +19,11 @@ const searchMobile = () => {
   }
 };
 const displayMobile = mobiles => {
-  const searchResult = document.getElementById("search-result");
   searchResult.textContent = "";
   if (mobiles.length == 0) {
     errorMassage.style.display = "block";
+    searchResult.textContent = "";
+    mobileDetails.textContent = "";
   } else {
     mobiles.forEach(mobile => {
       // console.log(mobile);
@@ -49,21 +54,36 @@ const showDetails = id => {
     .then(data => displayMobileDetails(data.data));
 };
 const displayMobileDetails = mobile => {
-  console.log(mobile.mainFeatures);
-  const mobileDetails = document.getElementById("display-mobile-details");
+  //   console.log(mobile.mainFeatures.sensors.toString());
+
   mobileDetails.textContent = "";
+  const releaseDate = () => {
+    if (mobile.releaseDate.length == 0) {
+      return "no releaseDate available";
+    } else {
+      return mobile.releaseDate;
+    }
+  };
+
   const div = document.createElement("div");
   div.innerHTML = `
-  <div class="card mx-auto p-2 h-100 w-75 ">
+  <div class="card mx-auto p-2 h-100 w-100 ">
         <div class="card-body">
-            <img src="${mobile.image}" class="card-img-top w-50 mx-auto" alt="..." />
+            <img src="${
+              mobile.image
+            }" class="card-img-top w-50 mx-auto" alt="..." />
             <h6 class="card-text">Name: ${mobile.name}</h6>
-            <h6 class="card-text">ReleaseDate: ${mobile.releaseDate}</h6>
+            <h6 class="card-text">ReleaseDate: ${releaseDate()}</h6>
             <h6 class="card-text">ChipSet: ${mobile.mainFeatures.chipSet}</h6>
-            <h6 class="card-text">DisplaySize: ${mobile.mainFeatures.displaySize}</h6>
+            <h6 class="card-text">DisplaySize: ${
+              mobile.mainFeatures.displaySize
+            }</h6>
             <h6 class="card-text">Memory: ${mobile.mainFeatures.memory}</h6>
             <h6 class="card-text">Storage: ${mobile.mainFeatures.storage}</h6>
-            
+            <h6 class="card-text">Bluetooth: ${mobile.others.Bluetooth}</h6>
+            <h6 class="card-text">GPS: ${mobile.others.GPS}</h6>
+            <h6 class="card-text">WLAN: ${mobile.others.WLAN}</h6>
+            <h6 class="card-text">Sensors: ${mobile.mainFeatures.sensors.toString()}</h6>
         </div>
   </div>
   `;
