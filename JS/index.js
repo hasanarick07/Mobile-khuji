b9a397ddd528252bff5b2bcc9de7ce36bb9eef5c
Arick Hasan <hasanarick000@gmail.com>
2022-03-02 20:30:19 +0600

    8th

--- JS/index.js
+++ JS/index.js
@@ -1,34 +1,46 @@
+const errorMassage = document.getElementById("error");
+errorMassage.style.display = "none";
 const searchMobile = () => {
   const mobileSearchField = document.getElementById("mobile-search-field");
   const searchText = mobileSearchField.value;
   mobileSearchField.value = "";
-  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-  fetch(url)
-    .then(res => res.json())
-    .then(data => displayMobile(data.data));
+  if (searchText == "") {
+    errorMassage.style.display = "block";
+  } else {
+    errorMassage.style.display = "none";
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    fetch(url)
+      .then(res => res.json())
+      .then(data => displayMobile(data.data));
+  }
 };
 const displayMobile = mobiles => {
   const searchResult = document.getElementById("search-result");
-  mobiles.forEach(mobile => {
-    // console.log(mobile);
-    const div = document.createElement("div");
-    div.classList.add("col");
-    div.innerHTML = `
-        <div class="card mt-3 mx-auto p-3 h-100 rounded">
-            <img src="${mobile.image}" class="card-img-top w-50 mx-auto" alt="..." />
-            <div class="card-body">
-              <h4 class="card-title">Brand: ${mobile.brand}</h4>
-              <h6 class="card-text">Name: ${mobile.phone_name}</h6>
-              <p>
-                <button onclick="showDetails('${mobile.slug}')"
-                  class="btn btn-primary">
-                  Details...
-                </button>
-              </p>
-            </div>
-          </div>`;
-    searchResult.appendChild(div);
-  });
+  searchResult.textContent = "";
+  if (mobiles.length == 0) {
+    errorMassage.style.display = "block";
+  } else {
+    mobiles.forEach(mobile => {
+      // console.log(mobile);
+      const div = document.createElement("div");
+      div.classList.add("col");
+      div.innerHTML = `
+            <div class="card mt-3 mx-auto p-3 h-100 rounded">
+                <img src="${mobile.image}" class="card-img-top w-50 mx-auto" alt="..." />
+                <div class="card-body">
+                  <h4 class="card-title">Brand: ${mobile.brand}</h4>
+                  <h6 class="card-text">Name: ${mobile.phone_name}</h6>
+                  <p>
+                    <button onclick="showDetails('${mobile.slug}')"
+                      class="btn btn-primary">
+                      Details...
+                    </button>
+                  </p>
+                </div>
+              </div>`;
+      searchResult.appendChild(div);
+    });
+  }
 };
 const showDetails = id => {
   const url = `https://openapi.programming-hero.com/api/phone/${id}`;
@@ -39,6 +51,7 @@ const showDetails = id => {
 const displayMobileDetails = mobile => {
   console.log(mobile.mainFeatures);
   const mobileDetails = document.getElementById("display-mobile-details");
+  mobileDetails.textContent = "";
   const div = document.createElement("div");
   div.innerHTML = `
   <div class="card mx-auto p-2 h-100 w-75 ">
